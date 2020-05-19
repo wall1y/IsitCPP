7fa35f2fc0dffa01779f939e007ce5cb1b842043
wall1y <larionov1994@gmail.com>
2020-05-20 01:43:15 +0500

    final

--- first.cpp
+++ first.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <regex>
 
 int main()
+
 {
-    std::cout << "Hello, world!" << std::endl;
+  std::string src;
+std:: ifstream fin("source") ;
+char ch;
+while (fin.get (ch)) // чтение символа из файла
+  src+=ch;
+fin.close () ;
+std::cout<< "Исходный текст: " << src << std::endl;
+std::string reverseText;
+//std::string line = "one? tw3o     three \n qwe qaz";
+std::stringstream aa(src);
+while (!aa.eof()) {
+  std::string s;
+  aa >> s;
+  std::string reverseWord;
+  for(int i=s.length()-1;i>=0;i--)
+    reverseWord+=s[i];
+  reverseText+=reverseWord + " ";
+
+}
+std::cout<< "Перевернутые слова: " << reverseText << std::endl;
     return 0;
 }
